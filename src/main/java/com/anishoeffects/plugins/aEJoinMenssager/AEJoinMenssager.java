51ba59beddf5cc859a79e66adac81345313f8cac
anishoeffects <edgardaxs78@gmail.com>
2025-07-24 14:04:28 -0300

    feat: Implement customizable welcome title

--- src/main/java/com/anishoeffects/plugins/aEJoinMenssager/AEJoinMenssager.java
+++ src/main/java/com/anishoeffects/plugins/aEJoinMenssager/AEJoinMenssager.java
@@ -1,23 +1,66 @@
 package com.anishoeffects.plugins.aEJoinMenssager;
 
 import com.anishoeffects.plugins.aEJoinMenssager.listener.JoinListener;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Map;
 
 
 public final class AEJoinMenssager extends JavaPlugin {
 
+    private Map<String, String> messages;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("Plugin started successfully!");
-        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+
+        // Verificando se diretorio (do plugin) já existe, caso não, cria o diretorio.
+        if (!getDataFolder().exists()) {
+            getLogger().info("Plugin folder not found, creating...");
+            getDataFolder().mkdirs();
+        } else {
+            getLogger().info("Plugin folder already exists!");
+        }
+        // Copiando o "messages.json" para a pasta plugins(se ele não existir)
+        File messageFile = new File(getDataFolder(), "messages.json");
+        if (!messageFile.exists()){
+            saveResource("messages.json", false);
+        }
+        // Carregando mensagens do arquivo para a memória
+        loadMessages();
+
+        getLogger().info("Plugin AEJoinMessage started successfully!");
+
+        getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+
+    }
+
+    private void loadMessages() {
+        File messageFile = new File(getDataFolder(), "messages.json");
+        try (FileReader reader = new FileReader(messageFile)) {
+            Type type = new TypeToken<Map<String, String>>(){}.getType();
+            this.messages = new Gson().fromJson(reader, type);
+        } catch (IOException e) {
+            getLogger().severe("Falha ao carregar messages.json");
+            this.messages = Collections.emptyMap(); // Evitar erros se o arquivo fahar.
+        }
 
 
+    }
+    public String getMessage(String key) {
+        return messages.getOrDefault(key, "");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        getLogger().info("Plugin disabled successfully!");
+        getLogger().info("Plugin AEJoinMessage disabled successfully!");
     }
 }
--- src/main/java/com/anishoeffects/plugins/aEJoinMenssager/listener/JoinListener.java
+++ src/main/java/com/anishoeffects/plugins/aEJoinMenssager/listener/JoinListener.java
@@ -1,9 +1,11 @@
 package com.anishoeffects.plugins.aEJoinMenssager.listener;
 
+import com.anishoeffects.plugins.aEJoinMenssager.AEJoinMenssager;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
 import net.kyori.adventure.title.Title;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,6 +14,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class JoinListener implements Listener {
+    private final AEJoinMenssager plugin;
+
+    public JoinListener(AEJoinMenssager plugin){
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onJoinListener(PlayerJoinEvent event) {
 
@@ -19,23 +27,27 @@ public class JoinListener implements Listener {
         Player player = event.getPlayer();
         // Criando mensagem de Boas Vindas.
         final TextComponent textWelcome = Component.text("Bem vindo ").color(TextColor.color(0xf7e1d7))
-                .append(Component.text(player.getName(), TextColor.color(0xedafb8))
-                        .append(Component.text(" ao Servidor!", TextColor.color(0xf7e1d7))));
+                .append(Component.text(player.getName(), TextColor.color(0xedafb8)));
 
         // Adicionando Mensagem ao Evento
         event.joinMessage(textWelcome);
 
         WelcomeTitle(player, player);
+
     }
 
     public void WelcomeTitle(@NotNull Audience target, Player player) {
-        // Criando meus componentes  de titulo
-        final Component mainTitle = Component.text("Bem vindo ", TextColor.color(0xf7e1d7))
-                .append(Component.text(player.getName(), TextColor.color(0xedafb8)));
-        final Component subtitle = Component.text("Ao servidor!", TextColor.color(0xf7e1d7));
+        // Pegando as strings JSON do arquivo carregado
+        String mainTitleJson = plugin.getMessage("welcome-title-main").replace("{player}", player.getName());
+        String subtitleJson = plugin.getMessage("welcome-title-subtitle");
+
+        // Deserializando as Strings
+
+        final Component mainTitle = JSONComponentSerializer.json().deserialize(mainTitleJson);
+        final Component subtitle = JSONComponentSerializer.json().deserialize(subtitleJson);
+
 
         // Criando o meu titulo
-
         final Title title = Title.title(mainTitle, subtitle);
 
         target.showTitle(title);
